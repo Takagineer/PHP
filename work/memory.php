0b948aea85f91f1f02474386b9124d2aaa582408
Takagineer <75421119+Takagineer@users.noreply.github.com>
2022-01-14 21:21:51 +0900

    共通ファイルの記述

---
+++ work/memory.php
@@ -0,0 +1,25 @@
+<?php 
+$title ='神経衰弱';
+ include ('_header.php');
+ ?>
+
+  <section class="works">
+    <h1>WORKS</h1>
+    <section>
+      <img src="img/work3.png" width="400" height="260" alt="勇者ゲームの紹介画像">
+      <h1>宝探し</h1>
+      <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
+      <a href="index.php">戻る</a>
+    </section>
+  
+
+  </section>
+
+  <footer>
+    <p>(c) dotinstall.com</p>
+  </footer>
+</body>
+</html>
+  
+<?php 
+ include ('_footer.php');
